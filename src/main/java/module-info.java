ff202ab0798a49f3a1828521ec38fbc7e2915328
Paul <berindeiepaul8@gmail.com>
2026-04-28 22:30:53 +0300

    fix more errors

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,9 +6,18 @@ module drinkshop {
     requires org.controlsfx.controls;
     requires java.desktop;
 
+    // 1. Give JavaFX access to your UI and Domain
     opens drinkshop.ui to javafx.fxml;
     exports drinkshop.ui;
 
-    opens drinkshop.domain to  javafx.base;
+    opens drinkshop.domain to javafx.base, org.mockito, net.bytebuddy;
     exports drinkshop.domain;
+
+    // 2. Add permissions for your Repositories and Services
+    // Mockito needs to "open" these to create the mock objects
+    opens drinkshop.repository to org.mockito, net.bytebuddy;
+    exports drinkshop.repository;
+
+    opens drinkshop.service to org.mockito, net.bytebuddy, org.junit.platform.commons;
+    exports drinkshop.service;
 }
